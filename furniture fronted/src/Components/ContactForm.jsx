74fdb6c27943b58be436356f7dc36c2c4589e0a0
SAGAR YADAV <sagaryadav15052005@gmail.com>
2024-09-01 02:23:33 -0700

    Update ContactForm.jsx

--- furniture fronted/src/Components/ContactForm.jsx
+++ furniture fronted/src/Components/ContactForm.jsx
@@ -10,9 +10,9 @@ function ContactForm() {
   // console.log(formData);
   //////////////////////////////////////////////
   let [formData1, setFromData1] = useState({ firstName: "hello" })
-  let [formData2, setFromData2] = useState({ lastName: "jangid" })
-  let [formData3, setFromData3] = useState({ email: "jangid@gmail.com" })
-  let [formData4, setFromData4] = useState({ message: "jangid hello" })
+  let [formData2, setFromData2] = useState({ lastName: "yadav" })
+  let [formData3, setFromData3] = useState({ email: "sagaryadav15052005@gmail.com" })
+  let [formData4, setFromData4] = useState({ message: "yadav hello" })
   // console.log(formData1["firstName"]);
 
   const handleChange = (e) => {
